e1ba193a858802a0fb3d848edb5a91bfd6ca1254
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2018-01-08 16:02:44 +0100

    Reduce the number of transform() calls

--- arguments.py
+++ arguments.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 import argparse
-
 from collections import defaultdict, Counter
+
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.pipeline import Pipeline
 from sklearn.metrics.pairwise import cosine_similarity as sim
+from sklearn.pipeline import Pipeline
 
 from roles import triples, clusters
 
@@ -23,26 +23,31 @@ subjects, predicates, objects = clusters(args.subjects), clusters(args.predicate
 
 D = []
 
-for dataset in (subjects, predicates, objects):
+for dataset in (subjects, objects):
     for cluster in dataset.values():
         D.append({element: 1 for element in cluster.elements})
 
 v = Pipeline([('dict', DictVectorizer()), ('tfidf', TfidfTransformer())])
-v.fit(D)
+X = v.fit_transform(D)
 
 del D
 
 subject_index, object_index = defaultdict(set), defaultdict(set)
+subject_vec, object_vec = {}, {}
 
-for cluster in subjects.values():
+for i, cluster in enumerate(subjects.values()):
     for element in cluster.elements:
         subject_index[element].add(cluster.id)
 
-for cluster in objects.values():
+    subject_vec[cluster.id] = i
+
+for i, cluster in enumerate(objects.values()):
     for element in cluster.elements:
         object_index[element].add(cluster.id)
 
-for cluster in predicates.values():
+    object_vec[cluster.id] = len(subject_vec) + i
+
+for x, cluster in enumerate(predicates.values()):
     predicate_triples = {spos[id] for predicate in cluster.elements for id in predicate_index[predicate]}
 
     subject_ctx = Counter([triple.subject for triple in predicate_triples])
@@ -51,18 +56,16 @@ for cluster in predicates.values():
     subject_space = {id for subject in subject_ctx for id in subject_index[subject]}
     object_space = {id for object in object_ctx for id in object_index[object]}
 
-    subject_sim = Counter({id: sim(v.transform(subject_ctx), v.transform({element: 1 for element in subjects[id].elements})).item(0) for id in subject_space})
-    object_sim = Counter({id: sim(v.transform(object_ctx), v.transform({element: 1 for element in objects[id].elements})).item(0) for id in object_space})
+    subject_sim = Counter({id: sim(v.transform(subject_ctx), X[subject_vec[id]]).item(0) for id in subject_space})
+    object_sim = Counter({id: sim(v.transform(object_ctx), X[object_vec[id]]).item(0) for id in object_space})
 
     print('# Cluster %s\n' % cluster.id)
     print('Predicates: %s' % ', '.join(cluster.elements))
 
     for id, cosine in subject_sim.most_common(1):
-        print('Subjects: %s' % ', '.join(subjects[id].elements))
+        print('Subjects (sim = %.4f): %s' % (cosine, ', '.join(subjects[id].elements)))
 
     for id, cosine in object_sim.most_common(1):
-        print('Objects: %s' % ', '.join(objects[id].elements))
+        print('Objects (sim = %.4f): %s' % (cosine, ', '.join(objects[id].elements)))
 
     print('')
-
-    pass
