86fd7faa95c05a39d2149792f3576e7702cce7eb
alexteua <oleksiy.oliynyk@gmail.com>
2018-09-18 16:21:04 +0300

    moved removing duplicates for entire dataset to a separate script #1

--- graph_embeddings/generate_graph.py
+++ graph_embeddings/generate_graph.py
@@ -6,9 +6,7 @@ import numpy as np
 import itertools
 import pickle
 
-
-lines_to_read = 170000
-lines_to_process = 100000
+lines_to_read = 100000
 
 def get_pairs(df, col1, col2):
     sub = df[df.duplicated(subset=[col1, col2], keep=False)]
@@ -27,13 +25,7 @@ def get_pairs(df, col1, col2):
 df = pd.read_csv("vso-1.3m-pruned-strict.csv", delimiter="\t", header=None,  nrows=lines_to_read)
 df.columns = ['verb', 'subject', 'object', 'score']
 df = df.reset_index()
-
-# add column with score sum over the duplicates
-df['sum'] = df.groupby(['verb', 'subject', 'object'])['score'].transform('sum')
-
-# drop duplicates
-df = df.drop_duplicates(subset=['verb', 'subject', 'object', 'sum'])[:lines_to_process]
-df = df.reset_index()
+df = df.fillna('')
 
 #init graph
 G=nx.Graph()
@@ -61,6 +53,7 @@ print ("edges: ", G.number_of_edges())
 
 #save graph
 nx.write_adjlist(G, "triframes.adjlist")
+nx.write_edgelist(G, "triframes.edgelist")
 
 #save dictionary with id mapping
 with open('id_to_name.pkl', 'wb') as f:
